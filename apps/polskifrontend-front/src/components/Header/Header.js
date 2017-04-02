f1736bf4000854a52c4f402cae4fd694d0ed9d9c
burczu <b.dybowski@gmail.com>
2017-04-03 00:35:48 +0200

    brand new logo

--- apps/polskifrontend-front/src/components/Header/Header.js
+++ apps/polskifrontend-front/src/components/Header/Header.js
@@ -2,13 +2,16 @@ import React from 'react';
 import withStyles from 'isomorphic-style-loader/lib/withStyles';
 import style from './Header.styl';
 import Link from '../Link/Link';
+import logo from '../../../public/polskifrontend.png';
 
 class Header extends React.Component {
   render() {
     return (
       <div className={style.header}>
         <Link className={style['header__link']} to="/">
-          <h1 className={style['header__title']}>Polski Front-End</h1>
+          <h1 className={style['header__title']}>
+            <img src={logo} />
+          </h1>
         </Link>
       </div>
     );
