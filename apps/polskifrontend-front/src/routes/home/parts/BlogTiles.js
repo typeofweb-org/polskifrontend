1cddaeb4b2338b309655037eafba88f23202e439
burczu <b.dybowski@gmail.com>
2017-04-11 19:06:09 +0200

    small margin fix

--- apps/polskifrontend-front/src/routes/home/parts/BlogTiles.js
+++ apps/polskifrontend-front/src/routes/home/parts/BlogTiles.js
@@ -17,7 +17,13 @@ const BlogTiles = props => {
               <section className={style['container__blog']}>
                 <h2 className={style.title}>
                   <a className={style['title__link']} target="_blank" href={item.href} rel="nofollow">
-                    {item.favicon !== '' ? <ReactImageFallback alt={`${item.name} - ikona`} className={style['title__favicon']} src={item.favicon} fallbackImage={noImage} /> : null}
+                    {item.favicon !== '' ?
+                      <ReactImageFallback alt={`${item.name} - ikona`}
+                                          className={style['title__favicon']}
+                                          src={item.favicon}
+                                          fallbackImage={noImage}
+                                          initialImage={noImage} />
+                      : null}
                     {item.name}
                   </a>
                 </h2>
