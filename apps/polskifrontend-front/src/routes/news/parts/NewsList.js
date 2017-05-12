1494ad605506d80d5217dcc448375372f0518740
burczu <b.dybowski@gmail.com>
2017-05-12 08:49:33 +0200

    news list margin fix

--- apps/polskifrontend-front/src/routes/news/parts/NewsList.js
+++ apps/polskifrontend-front/src/routes/news/parts/NewsList.js
@@ -10,45 +10,41 @@ import * as dateHelper from '../../../core/helpers/dateHelper';
 
 const NewsList = (props) => {
   return (
-    <div>
-      <ResponsivePanel className={styles.container} header="Aktualności" description="">
-        <div className={styles.nav}>
-          <Link className={styles.back} to="/">
-            <i className="fa fa-arrow-left">
-            </i>
-            Strona główna
-          </Link>
-        </div>
-        {props.newsList.map((item, index) => {
-          const isTodayArticle = dateHelper.isToday(new Date(item.date));
-          const newClass = `${styles['item__new']} ${isTodayArticle ? styles['item__new--visible'] : ''}`;
+    <ResponsivePanel className={styles.container} header="Aktualności" description="">
+      <div className={styles.nav}>
+        <Link className={styles.back} to="/">
+          <i className="fa fa-arrow-left">
+          </i>
+          Strona główna
+        </Link>
+      </div>
+      {props.newsList.map((item, index) => {
+        const isTodayArticle = dateHelper.isToday(new Date(item.date));
+        const newClass = `${styles['item__new']} ${isTodayArticle ? styles['item__new--visible'] : ''}`;
 
-          return (
-            <div key={index}>
-              <div className={styles.item}>
-                <h2 className={styles['item__header']}>
-                  <span className={newClass}>Nowe</span>
-                  {item.title}
-                </h2>
-                <div className={styles['meta']}>
-                  <p className={styles['meta__date']}>
-                    {dateFormat(item.date, 'dd-mm-yyyy')}
-                  </p>
-                  <pre className={styles['meta__message']}>
-                    {item.message}
-                  </pre>
-                </div>
-              </div>
+        return (
+          <div className={styles.item} key={index}>
+            <h2 className={styles['item__header']}>
+              <span className={newClass}>Nowe</span>
+              {item.title}
+            </h2>
+            <div className={styles['meta']}>
+              <p className={styles['meta__date']}>
+                {dateFormat(item.date, 'dd-mm-yyyy')}
+              </p>
+              <pre className={styles['meta__message']}>
+                {item.message}
+              </pre>
             </div>
-          );
-        })}
-        {props.nextPage !== -1 ? (
-          <Loader isLoading={props.isLoadingMore}>
-            <Waypoint onEnter={props.onScrolledBottom} scrollableAncestor="window" />
-          </Loader>
-        ) : null}
-      </ResponsivePanel>
-    </div>
+          </div>
+        );
+      })}
+      {props.nextPage !== -1 ? (
+        <Loader isLoading={props.isLoadingMore}>
+          <Waypoint onEnter={props.onScrolledBottom} scrollableAncestor="window" />
+        </Loader>
+      ) : null}
+    </ResponsivePanel>
   );
 };
 
