ff7301564b45d68a69d5124e4cdb8082355e93a2
burczu <b.dybowski@gmail.com>
2017-11-15 15:56:09 +0100

    config removed (example added)

---
+++ apps/polskifrontend-back/src/config.example.js
@@ -0,0 +1,36 @@
+const env = process.env.NODE_ENV;
+const common = {
+  port: 8880,
+  cloudinary: {
+    cloud_name: 'cloudinary_name',
+    api_key: 'XXXXXXXXXXXXXXX',
+    api_secret: 'XXXXXXXXXXXXXXXXXXXXXXXXXXX'
+  }
+};
+const config = {
+  develop: {
+    mongodb: {
+      host: '127.0.0.1',
+      database: 'local_database_name'
+    },
+    secret: 'some_secret'
+  },
+  production: {
+    mongodb: {
+      host1: 'remote_database_host1',
+      host2: 'remote_database_host2',
+      database: 'remote_database_name',
+      user: 'remote_database_user',
+      password: 'remote_database_password',
+      port: 'remote_database_port'
+    },
+    secret: 'remote_database_secret'
+  },
+  test: {
+    mongodb: {
+      host: '127.0.0.1',
+      database: 'local_database_name'
+    }
+  }
+};
+export default Object.assign(common, config[env]);
