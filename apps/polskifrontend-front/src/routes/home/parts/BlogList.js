42ccf9daf224f0e524818390ba8d2ba9e786e81d
burczu <b.dybowski@gmail.com>
2017-11-15 16:09:20 +0100

    migrate to PropTypes as a standalone lib

--- apps/polskifrontend-front/src/routes/home/parts/BlogList.js
+++ apps/polskifrontend-front/src/routes/home/parts/BlogList.js
@@ -1,4 +1,5 @@
-import React, { PropTypes } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 import withStyles from 'isomorphic-style-loader/lib/withStyles';
 import styles from './BlogList.styl';
 import ResponsivePanel from '../../../components/Responsive/ResponsivePanel';
