c8c65d3b2d6a848c46d1b2de9727c2f1996148ff
adamklepacz <adamklepacz92@gmail.com>
2020-12-09 12:16:49 +0100

    Update footer date with current year

--- apps/polskifrontend-front/src/components/Footer/Footer.js
+++ apps/polskifrontend-front/src/components/Footer/Footer.js
@@ -1,12 +1,23 @@
-import React from 'react';
-import withStyles from 'isomorphic-style-loader/lib/withStyles';
-import styles from './Footer.styl';
+import React from "react";
+import withStyles from "isomorphic-style-loader/lib/withStyles";
+import styles from "./Footer.styl";
 
 class Footer extends React.Component {
   render() {
+    const currentYear = new Date().getFullYear();
+
     return (
       <div className={styles.footer}>
-        <p>Copyright@2017 - <a className={styles['footer__link']} target="_blank" href="https://nafrontendzie.pl">Na Frontendzie</a></p>
+        <p>
+          {`Copyright@2017&nbsp;-&nbsp;${currentYear}`}
+          <a
+            className={styles["footer__link"]}
+            target="_blank"
+            href="https://nafrontendzie.pl"
+          >
+            Na Frontendzie
+          </a>
+        </p>
       </div>
     );
   }
