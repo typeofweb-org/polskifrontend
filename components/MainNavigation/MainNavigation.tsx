19fc50de9994850714575b783bd5f9468b99035d
Michal Miszczyszyn <michal@mmiszy.pl>
2020-12-18 18:04:53 +0100

    CR

--- components/MainNavigation/MainNavigation.tsx
+++ components/MainNavigation/MainNavigation.tsx
@@ -3,13 +3,13 @@ import Link from 'next/link';
 import styles from './mainNavigation.module.scss';
 
 const links = [
-  { label: 'Aktualności', href: '/aktualnosci', openInNewWindow: false, icon: 'inbox' },
-  { label: 'O serwisie', href: '/o-serwisie', openInNewWindow: false, icon: 'question-circle' },
-  { label: 'Zgłoś uwagi', href: '/zglos-uwagi', openInNewWindow: false, icon: 'comment' },
+  { label: 'Aktualności', href: '/aktualnosci', openInNewTab: false, icon: 'inbox' },
+  { label: 'O serwisie', href: '/o-serwisie', openInNewTab: false, icon: 'question-circle' },
+  { label: 'Zgłoś uwagi', href: '/zglos-uwagi', openInNewTab: false, icon: 'comment' },
   {
     label: 'Facebook',
     href: 'https://facebook.com/polskifrontend',
-    openInNewWindow: true,
+    openInNewTab: true,
     icon: 'facebook',
   },
 ];
@@ -18,10 +18,10 @@ export const MainNavigation = () => {
   return (
     <nav>
       <ul className={styles.navigationList}>
-        {links.map(({ label, href, openInNewWindow, icon }) => (
+        {links.map(({ label, href, openInNewTab, icon }) => (
           <li className={styles.navigationItem} key={href}>
             <Link href={href}>
-              <a {...(openInNewWindow && { target: '_blank' })} title={label}>
+              <a {...(openInNewTab && { target: '_blank' })} title={label}>
                 <span className={`icon- ${styles.icon}`}>{icon}</span>
                 <span className={styles.label}>{label}</span>
               </a>
