d30805ebd954607639974aac6b2d4f0bf8207d05
Michal Miszczyszyn <michal@mmiszy.pl>
2020-12-18 18:05:54 +0100

    Add data fetching
    Update max-width

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,54 @@
+import type { InferGetStaticPropsType } from 'next';
+
+import { closeConnection, openConnection } from '../api-helpers/db';
 import { Layout } from '../components/Layout';
 
-export default function HomePage() {
+export default function HomePage({ blogs }: InferGetStaticPropsType<typeof getStaticProps>) {
   return (
     <Layout>
       <h1>Siema!</h1>
+      <ul>
+        {blogs.map((blog) => (
+          <li key={blog.id}>
+            {blog.name}
+            <ul>
+              {blog.articles.map((article) => (
+                <li key={article.id}>{article.title}</li>
+              ))}
+            </ul>
+          </li>
+        ))}
+      </ul>
     </Layout>
   );
 }
+
+const BLOGS_PER_PAGE = 4;
+const ARTICLES_PER_BLOG = 5;
+const REVALIDATION_TIME = 15 * 60; // 15 minutes
+
+export const getStaticProps = async () => {
+  try {
+    const prisma = await openConnection();
+
+    // data for tiles/grid
+    const blogs = await prisma.blog.findMany({
+      take: BLOGS_PER_PAGE,
+      orderBy: {
+        lastUpdateDate: 'desc',
+      },
+      include: {
+        articles: {
+          take: ARTICLES_PER_BLOG,
+          orderBy: {
+            publishedAt: 'desc',
+          },
+        },
+      },
+    });
+
+    return { props: { blogs }, revalidate: REVALIDATION_TIME };
+  } finally {
+    await closeConnection();
+  }
+};
