17e759170e517db415a8a5136b1b2e2b140f3955
Adam Siekierski <a@siekierski.ml>
2020-12-19 16:48:48 +0100

    Fix useDidMount

--- hooks/useDidMount.tsx
+++ hooks/useDidMount.tsx
@@ -1,4 +1,4 @@
 import type { EffectCallback } from 'react';
 import { useEffect } from 'react';
 
-export const useDidMount = (cb: EffectCallback) => useEffect(cb, []);
+export const useDidMount = (cb: EffectCallback) => useEffect(cb, [cb]);
