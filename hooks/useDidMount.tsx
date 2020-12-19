1e52dc30091e0db44d55d9750516f3a54d7dfb8e
Adam Siekierski <adam.siekiera@outlook.com>
2020-12-19 17:07:42 +0100

    Fix useDidMount

--- hooks/useDidMount.tsx
+++ hooks/useDidMount.tsx
@@ -2,4 +2,4 @@ import type { EffectCallback } from 'react';
 import { useEffect } from 'react';
 
 // eslint-ignore
-export const useDidMount = (cb: EffectCallback) => useEffect(cb);
+export const useDidMount = (cb: EffectCallback) => useEffect(cb, []);
