57aded5ec6de233298c814ae556deb883c383779
Adam Siekierski <a@siekierski.ml>
2020-12-23 18:31:33 +0100

    Create blogs grid

--- components/MainTiles/BlogsGrid.tsx
+++ components/MainTiles/BlogsGrid.tsx
@@ -1,7 +1,9 @@
-import { memo } from 'react';
+import { Fragment, memo } from 'react';
 
 import type { HomePageProps } from '../../pages/[displayStyle]/[cursor]';
 import { ArticleTile } from '../ArticleTile/ArticleTile';
+
+import styles from './blogsGrid.module.scss';
 
 type BlogsGridProps = {
   readonly blogs: NonNullable<HomePageProps['blogs']>;
@@ -9,18 +11,29 @@ type BlogsGridProps = {
 
 export const BlogsGrid = memo<BlogsGridProps>(({ blogs }) => {
   return (
-    <ul style={{ listStyleType: 'none', padding: 0, margin: 0 }}>
-      {blogs.map((blog) => (
-        <li key={blog.id}>
-          {blog.name}
-          <ul style={{ listStyleType: 'none', padding: 0, margin: 0 }}>
-            {blog.articles.map((article) => (
-              <li key={article.id}>
-                <ArticleTile article={article} blog={blog} />
-              </li>
-            ))}
-          </ul>
-        </li>
+    <ul className={styles.blogsGrid}>
+      {blogs.map((blog, i) => (
+        <Fragment key={blog.id}>
+          <li className={styles.gridItem}>
+            <h3 className={styles.blogHeader}>
+              <a href={blog.href} target="_blank" rel="noreferrer">
+                <img
+                  src={blog.favicon || undefined}
+                  alt=""
+                  className={styles.favicon}
+                  height={26}
+                  width={26}
+                />
+                {' ' + blog.name}
+              </a>
+            </h3>
+          </li>
+          {blog.articles.map((article) => (
+            <li key={article.id} className={styles.gridItem}>
+              <ArticleTile article={article} blog={blog} />
+            </li>
+          ))}
+        </Fragment>
       ))}
     </ul>
   );
