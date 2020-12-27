7a8812adad56747348809cc331f915059b079472
Michal Miszczyszyn <michal@mmiszy.pl>
2020-12-27 15:48:53 +0100

    Add safe paddings to ios safari

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -43,6 +43,10 @@ export default function MyApp({ Component, pageProps }: AppProps) {
       />
       {/* Tags for favicon and manifest from favicon-generator.com */}
       <Head>
+        <meta
+          name="viewport"
+          content="width=device-width, user-scalable=yes, initial-scale=1.0, viewport-fit=cover"
+        />
         <link rel="apple-touch-icon" sizes="57x57" href="/apple-icon-57x57.png" />
         <link rel="apple-touch-icon" sizes="60x60" href="/apple-icon-60x60.png" />
         <link rel="apple-touch-icon" sizes="72x72" href="/apple-icon-72x72.png" />
