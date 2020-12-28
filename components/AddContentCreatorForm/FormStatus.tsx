d15fc00c900e44902310100949184c4ddceb597c
Bartłomiej Wiśniewski <b.wisniewski128@gmail.com>
2020-12-28 13:43:11 +0100

    Resolve known issues (#63)
    * Resolve known issues
    
    * Improve form UX
    
    Co-authored-by: Michał Miszczyszyn <mmiszy@users.noreply.github.com>

---
+++ components/AddContentCreatorForm/FormStatus.tsx
@@ -0,0 +1,55 @@
+import clsx from 'clsx';
+import { memo } from 'react';
+
+import type { Status } from '../../hooks/useAddContentCreatorMutation';
+
+import styles from './FormStatus.module.css';
+
+type Props = {
+  readonly status: Status;
+};
+
+export const FormStatus = memo<Props>(({ status }) => {
+  switch (status) {
+    case 'loading':
+      return (
+        <div className={styles.statusContainer}>
+          <span className={clsx(styles.statusIcon, 'icon-spinner')}></span>
+          <span>Oczekiwanie...</span>
+        </div>
+      );
+    case 'error':
+      return (
+        <div className={styles.statusContainer}>
+          <span className={clsx(styles.statusIcon, 'icon-error')}></span>
+          <span>
+            Wystąpił błąd podczas dodawania nowego serwisu, sprawdź poprawność danych i spróbuj
+            ponownie
+          </span>
+        </div>
+      );
+    case 'notRssFound':
+      return (
+        <div className={styles.statusContainer}>
+          <span className={clsx(styles.statusIcon, 'icon-error')}></span>
+          <span>
+            Nie udało się odnaleźć pliku rss na twojej stronie, jeśli ten błąd się powtarza, proszę
+            skontaktuj się z administratorem.
+          </span>
+        </div>
+      );
+    case 'success':
+      return (
+        <div className={styles.statusContainer}>
+          <span className={clsx(styles.statusIcon, 'icon-checkmark')}></span>
+          <span>
+            Dziękujemy za zgłoszenie, dodany serwis pojawi się na stronie po zaakceptowaniu przez
+            administrację
+          </span>
+        </div>
+      );
+    default:
+      return null;
+  }
+});
+FormStatus.displayName = 'FormStatus';
