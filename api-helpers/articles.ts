cf0ed398d1db40db1dd74ea29e320132f66ae954
Adam Siekierski <a@siekierski.ml>
2020-12-28 11:27:33 +0100

    self-cr

--- api-helpers/articles.ts
+++ api-helpers/articles.ts
@@ -123,7 +123,7 @@ export const getArticlesPaginationForList = async (prisma: PrismaClient) => {
   return cursors;
 };
 
-export const getArticles = async (prisma: PrismaClient, limit: number | undefined = undefined) => {
+export const getArticles = async (prisma: PrismaClient, limit?: number) => {
   const articles = await prisma.article.findMany({
     orderBy: {
       createdAt: 'desc',
