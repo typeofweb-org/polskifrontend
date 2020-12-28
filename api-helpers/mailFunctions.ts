4469bee138e27d4f56e6a7fafc706959e2885654
Adam Siekierski <a@siekierski.ml>
2020-12-29 00:56:58 +0100

    Add mail notification on new creator (#67)
    * Add mail notification on new creator
    
    * CR

---
+++ api-helpers/mailFunctions.ts
@@ -0,0 +1,59 @@
+import type { Blog } from '@prisma/client';
+import { UserRole } from '@prisma/client';
+import Mailgun from 'mailgun-js';
+
+import { closeConnection, openConnection } from './db';
+
+const mg = Mailgun({ apiKey: process.env.MAILGUN_API_KEY!, domain: process.env.MAILGUN_DOMAIN! });
+
+const sendEmail = (
+  recepients: readonly string[],
+  subject: string,
+  text: string,
+  replyTo?: string,
+) =>
+  mg.messages().send({
+    from: process.env.MAILGUN_FROM!,
+    to: recepients.join(', '),
+    subject,
+    text,
+    'h:Reply-To': replyTo,
+  });
+
+const getNewCreatorEmail = (blog: Record<string, string>) => {
+  return `
+Nowy wniosek o dodanie serwisu!
+
+Dane:
+${Object.entries(blog)
+  .map((entry) => `${entry[0]}: ${entry[1].trim()}`)
+  .join('\n')
+  .trim()}
+
+*Tutaj damy jakiegoś linka czy coś do akceptacji*
+`;
+};
+
+export const sendNewCreatorNotification = async ({ name, href, rss, creatorEmail }: Blog) => {
+  try {
+    const prisma = await openConnection();
+
+    const admins = await prisma.user.findMany({
+      where: {
+        role: UserRole.ADMIN,
+      },
+      select: {
+        email: true,
+      },
+    });
+
+    await sendEmail(
+      admins.map((a) => a.email),
+      `Polski Frontend | Nowy serwis | ${name}`,
+      getNewCreatorEmail({ name, href, rss, creatorEmail: creatorEmail || '' }),
+      creatorEmail || undefined,
+    );
+  } finally {
+    await closeConnection();
+  }
+};
