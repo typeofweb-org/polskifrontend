abb7b53757d0768ad6e8d7be5fa9171da5efa0a0
Adam Siekierski <a@siekierski.ml>
2021-01-01 18:09:31 +0100

    feat: Dodano wyjątek dla już dodanego bloga w formularzu (#94)
    * Add message for already added blog
    
    * CR
    
    * CR
    
    * fix
    
    * cr

--- components/AddContentCreatorForm/FormStatus.tsx
+++ components/AddContentCreatorForm/FormStatus.tsx
@@ -1,55 +1,52 @@
 import clsx from 'clsx';
 import { memo } from 'react';
 
-import type { Status } from '../../hooks/useAddContentCreatorMutation';
+import type { Status } from '../../hooks/useMutation';
 
 import styles from './FormStatus.module.css';
 
 type Props = {
   readonly status: Status;
+  readonly errorCode?: number;
 };
 
-export const FormStatus = memo<Props>(({ status }) => {
+const errorToMessage: Record<number, string> = {
+  409: 'Blog o podanym adresie został już dodany do naszej bazy danych. Jeżeli nie pojawiają się najnowsze wpisy, lub masz inne zastrzeżenia - proszę skontaktuj się z administratorem.',
+  422: 'Nie udało się odnaleźć pliku RSS na twojej stronie, jeśli ten błąd się powtarza, proszę skontaktuj się z administratorem.',
+};
+
+const defaultErrorMessage =
+  'Wystąpił błąd podczas dodawania nowego serwisu, sprawdź poprawność danych i spróbuj ponownie';
+
+function getStatusMessage({ status, errorCode }: Props) {
   switch (status) {
     case 'loading':
-      return (
-        <div className={styles.statusContainer}>
-          <span className={clsx(styles.statusIcon, 'icon-spinner')}></span>
-          <span>Oczekiwanie...</span>
-        </div>
-      );
-    case 'error':
-      return (
-        <div className={styles.statusContainer}>
-          <span className={clsx(styles.statusIcon, 'icon-error')}></span>
-          <span>
-            Wystąpił błąd podczas dodawania nowego serwisu, sprawdź poprawność danych i spróbuj
-            ponownie
-          </span>
-        </div>
-      );
-    case 'notRssFound':
-      return (
-        <div className={styles.statusContainer}>
-          <span className={clsx(styles.statusIcon, 'icon-error')}></span>
-          <span>
-            Nie udało się odnaleźć pliku rss na twojej stronie, jeśli ten błąd się powtarza, proszę
-            skontaktuj się z administratorem.
-          </span>
-        </div>
-      );
+      return 'Oczekiwanie...';
     case 'success':
-      return (
-        <div className={styles.statusContainer}>
-          <span className={clsx(styles.statusIcon, 'icon-checkmark')}></span>
-          <span>
-            Dziękujemy za zgłoszenie, dodany serwis pojawi się na stronie po zaakceptowaniu przez
-            administrację
-          </span>
-        </div>
-      );
+      return 'Dziękujemy za zgłoszenie, dodany serwis pojawi się na stronie po zaakceptowaniu przez administrację';
+    case 'error':
+      return errorCode ? errorToMessage[errorCode] ?? defaultErrorMessage : defaultErrorMessage;
     default:
       return null;
   }
+}
+
+export const FormStatus = memo<Props>(({ status, errorCode }) => {
+  if (status === 'idle') {
+    return null;
+  }
+
+  return (
+    <div className={styles.statusContainer}>
+      <span
+        className={clsx(styles.statusIcon, {
+          'icon-spinner': status === 'loading',
+          'icon-checkmark': status === 'success',
+          'icon-error': status === 'error',
+        })}
+      ></span>
+      <span>{getStatusMessage({ status, errorCode })}</span>
+    </div>
+  );
 });
 FormStatus.displayName = 'FormStatus';
