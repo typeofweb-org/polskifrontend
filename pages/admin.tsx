bd0c75e52da5381be23f28229e022138a2ff2606
Bartłomiej Wiśniewski <b.wisniewski128@gmail.com>
2021-01-02 17:14:37 +0100

    feat: Dodano możliwość autoryzacji użytkowników (#87)
    * Code review
    
    * Code review
    
    * Fix admin panel
    
    * Delete unused code
    
    * Code review
    
    * Code review
    
    * Fix dynamic import issue
    
    * Delete next-auth provider
    
    * Fix type annotation
    
    * Delete type annotation
    
    Co-authored-by: Michał Miszczyszyn <mmiszy@users.noreply.github.com>

---
+++ pages/admin.tsx
@@ -0,0 +1,17 @@
+import dynamic from 'next/dynamic';
+
+import { Layout } from '../components/Layout';
+
+const AdminPanel = dynamic<{}>(() =>
+  import(/* webpackChunkName: "AdminPanel" */ '../components/AdminPanel/AdminPanel').then(
+    (mod) => mod.AdminPanel,
+  ),
+);
+
+export default function AdminPage() {
+  return (
+    <Layout title="Panel admina">
+      <AdminPanel />
+    </Layout>
+  );
+}
