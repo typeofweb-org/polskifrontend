1d700367583d12cd044d46d81f120184b20407db
Michal Miszczyszyn <michal@mmiszy.pl>
2021-01-06 20:23:30 +0100

    Refactor hofs

--- pages/api/content-creator.ts
+++ pages/api/content-creator.ts
@@ -2,36 +2,36 @@ import Boom from '@hapi/boom';
 import { verify } from 'hcaptcha';
 import { string, object } from 'yup';
 
-import { withAsync, withValidation } from '../../api-helpers/api-hofs';
+import { withAsync, withDb, withMethods, withValidation } from '../../api-helpers/api-hofs';
 import { addContentCreator } from '../../api-helpers/contentCreatorFunctions';
 import { sendNewCreatorNotification } from '../../api-helpers/mailFunctions';
 
 export default withAsync(
-  withValidation({
-    body: object({
-      email: string().email().required(),
-      contentURL: string().url().required(),
-      captchaToken: string().required(),
-    }).required(),
-  })(async (req) => {
-    if (req.method !== 'POST') {
-      throw Boom.notFound();
-    }
+  withMethods({
+    POST: withValidation({
+      body: object({
+        email: string().email().required(),
+        contentURL: string().url().required(),
+        captchaToken: string().required(),
+      }).required(),
+    })(
+      withDb(async (req) => {
+        const isTokenValid = await verify(
+          process.env.CAPTCHA_SECRET_KEY as string,
+          req.body.captchaToken,
+        );
+        if (!isTokenValid) {
+          throw Boom.unauthorized();
+        }
 
-    const isTokenValid = await verify(
-      process.env.CAPTCHA_SECRET_KEY as string,
-      req.body.captchaToken,
-    );
-    if (!isTokenValid) {
-      throw Boom.unauthorized();
-    }
+        // normalize url
+        const url = new URL(req.body.contentURL).toString();
 
-    // normalize url
-    const url = new URL(req.body.contentURL).toString();
+        const contentCreator = await addContentCreator(url, req.body.email, req.db);
+        await sendNewCreatorNotification(contentCreator, req.db).catch(() => {});
 
-    const contentCreator = await addContentCreator(url, req.body.email);
-    await sendNewCreatorNotification(contentCreator).catch(() => {});
-
-    return null;
+        return null;
+      }),
+    ),
   }),
 );
