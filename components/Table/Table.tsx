f09f5ee85336f5fec1cf976ed658989e828dd05c
Michal Miszczyszyn <michal@mmiszy.pl>
2021-01-07 19:42:16 +0100

    table poc

--- components/Table/Table.tsx
+++ components/Table/Table.tsx
@@ -1,3 +1,5 @@
+import { useEffect, useMemo, useRef } from 'react';
+
 import styles from './table.module.scss';
 
 type TableProps<T> = {
@@ -5,25 +7,111 @@ type TableProps<T> = {
   readonly columns: ReadonlyArray<readonly [key: keyof T, label: string]>;
 };
 
-export const Table = <T extends { readonly id: string }>({ data, columns }: TableProps<T>) => (
-  <div className={styles.tableWrapper}>
-    <table className={styles.table}>
-      <thead>
-        <tr>
-          {columns.map(([key, label]) => (
-            <th key={key as string}>{label}</th>
-          ))}
-        </tr>
-      </thead>
-      <tbody>
-        {data.map((row) => (
-          <tr key={row.id}>
-            {columns.map(([key]) => (
-              <td key={key as string}>{row[key]}</td>
+const rafThrottle = <T extends readonly unknown[]>(
+  fn: (...args: T) => any,
+): ((...args: T) => void) => {
+  if (typeof window === 'undefined') {
+    return () => {};
+  }
+  const fnToThrottle = window.requestIdleCallback || window.requestAnimationFrame;
+
+  let isPainting = false;
+  return (...args) => {
+    if (isPainting) {
+      return;
+    }
+    isPainting = true;
+    fnToThrottle(
+      () => {
+        fn(...args);
+        setTimeout(() => (isPainting = false));
+      },
+      { timeout: 100 },
+    );
+  };
+};
+
+const cssPropsToCopy = [
+  'display',
+  'verticalAlign',
+  'textAlign',
+  'font',
+  'background',
+  'borderTop',
+  'borderRight',
+  'borderBottom',
+  'borderLeft',
+  'padding',
+] as const;
+
+const useFloatingTableHeader = <T extends HTMLElement>() => {
+  const tableRef = useRef<HTMLTableElement>(null);
+  const targetRef = useRef<T>(null);
+
+  const update = useMemo(
+    () =>
+      rafThrottle(() => {
+        if (!tableRef.current || !targetRef.current) {
+          return;
+        }
+
+        const headers = tableRef.current.querySelectorAll('thead th');
+        const newElements = Array.from(headers).map((th) => {
+          const thRect = th.getBoundingClientRect();
+          const thStyle = getComputedStyle(th);
+          const span = document.createElement('span');
+          span.innerHTML = th.innerHTML;
+          span.style.height = `${thRect.height}px`;
+          span.style.width = `${thRect.width}px`;
+          cssPropsToCopy.forEach((prop) => (span.style[prop] = thStyle[prop]));
+          return span;
+        });
+
+        const tableRect = tableRef.current.getBoundingClientRect();
+        targetRef.current.innerHTML = '';
+        targetRef.current.style.zIndex = '1';
+        targetRef.current.style.position = 'absolute';
+        targetRef.current.style.width = `${tableRect.width}px`;
+        newElements.forEach((el) => targetRef.current?.appendChild(el));
+
+        console.log(tableRef.current, targetRef.current);
+      }),
+    [],
+  );
+
+  useEffect(() => {
+    update();
+
+    window.addEventListener('resize', update, { passive: true });
+    return () => window.removeEventListener('resize', update);
+  });
+
+  return { tableRef, targetRef };
+};
+
+export const Table = <T extends { readonly id: string }>({ data, columns }: TableProps<T>) => {
+  const { tableRef, targetRef } = useFloatingTableHeader<HTMLDivElement>();
+  return (
+    <div className={styles.tableWrapper}>
+      <div ref={targetRef} />
+      <table ref={tableRef} className={styles.table}>
+        <thead>
+          <tr>
+            {columns.map(([key, label]) => (
+              <th key={key as string}>{label}</th>
             ))}
           </tr>
-        ))}
-      </tbody>
-    </table>
-  </div>
-);
+        </thead>
+        <tbody>
+          {data.map((row) => (
+            <tr key={row.id}>
+              {columns.map(([key]) => (
+                <td key={key as string}>{row[key]}</td>
+              ))}
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
--- pages/api/blogs/index.ts
+++ pages/api/blogs/index.ts
@@ -4,27 +4,25 @@ import { boolean, object } from 'yup';
 import { withAsync, withValidation, withAuth, withDb } from '../../../api-helpers/api-hofs';
 
 export default withAsync(
-  withAuth('ADMIN')(
-    withValidation({
-      query: object({
-        isPublic: boolean().optional(),
-      }).optional(),
-    })(
-      withDb(async (req) => {
-        if (req.method !== 'GET') {
-          throw Boom.notFound();
-        }
+  withValidation({
+    query: object({
+      isPublic: boolean().optional(),
+    }).optional(),
+  })(
+    withDb(async (req) => {
+      if (req.method !== 'GET') {
+        throw Boom.notFound();
+      }
 
-        const blogs = await req.db.blog.findMany({
-          where: {
-            isPublic: req.query.isPublic,
-          },
-        });
+      const blogs = await req.db.blog.findMany({
+        where: {
+          isPublic: req.query.isPublic,
+        },
+      });
 
-        return {
-          data: blogs,
-        };
-      }),
-    ),
+      return {
+        data: blogs,
+      };
+    }),
   ),
 );
