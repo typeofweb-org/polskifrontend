632d905ac7d2c5be8cda9bede754838a5b99d810
Michal Miszczyszyn <michal@mmiszy.pl>
2021-01-08 14:24:41 +0100

    Uproszczenie

--- components/Table/Table.tsx
+++ components/Table/Table.tsx
@@ -1,112 +1,52 @@
-import { useEffect, useMemo, useRef } from 'react';
+import type { CSSProperties } from 'react';
 
 import styles from './table.module.scss';
+import { useFloatingTableHeader } from './useFloatingTableHeader';
 
-type TableProps<T> = {
+type TableProps<T extends { readonly id: string } = { readonly id: string }> = {
   readonly data: readonly T[];
-  readonly columns: ReadonlyArray<readonly [key: keyof T, label: string]>;
+  readonly columns: ReadonlyArray<readonly [key: keyof T & string, label: string]>;
 };
 
-const rafThrottle = <T extends readonly unknown[]>(
-  fn: (...args: T) => any,
-): ((...args: T) => void) => {
-  if (typeof window === 'undefined') {
-    return () => {};
-  }
-  const fnToThrottle = window.requestIdleCallback || window.requestAnimationFrame;
-
-  let isPainting = false;
-  return (...args) => {
-    if (isPainting) {
-      return;
-    }
-    isPainting = true;
-    fnToThrottle(
-      () => {
-        fn(...args);
-        setTimeout(() => (isPainting = false));
-      },
-      { timeout: 100 },
-    );
-  };
-};
-
-const cssPropsToCopy = [
-  'display',
-  'verticalAlign',
-  'textAlign',
-  'font',
-  'background',
-  'borderTop',
-  'borderRight',
-  'borderBottom',
-  'borderLeft',
-  'padding',
-] as const;
-
-const useFloatingTableHeader = <T extends HTMLElement>() => {
-  const tableRef = useRef<HTMLTableElement>(null);
-  const targetRef = useRef<T>(null);
-
-  const update = useMemo(
-    () =>
-      rafThrottle(() => {
-        if (!tableRef.current || !targetRef.current) {
-          return;
-        }
-
-        const headers = tableRef.current.querySelectorAll('thead th');
-        const newElements = Array.from(headers).map((th) => {
-          const thRect = th.getBoundingClientRect();
-          const thStyle = getComputedStyle(th);
-          const span = document.createElement('span');
-          span.innerHTML = th.innerHTML;
-          span.style.height = `${thRect.height}px`;
-          span.style.width = `${thRect.width}px`;
-          cssPropsToCopy.forEach((prop) => (span.style[prop] = thStyle[prop]));
-          return span;
-        });
-
-        const tableRect = tableRef.current.getBoundingClientRect();
-        targetRef.current.innerHTML = '';
-        targetRef.current.style.zIndex = '1';
-        targetRef.current.style.position = 'absolute';
-        targetRef.current.style.width = `${tableRect.width}px`;
-        newElements.forEach((el) => targetRef.current?.appendChild(el));
-
-        console.log(tableRef.current, targetRef.current);
-      }),
-    [],
+const TableHeaderRow = ({
+  as: As,
+  columns,
+  styles,
+}: {
+  readonly as: 'span' | 'th';
+  readonly styles?: readonly CSSProperties[];
+  readonly columns: readonly (readonly [string, string])[];
+}) => {
+  return (
+    <>
+      {columns.map(([key, label], index) => (
+        <As style={styles?.[index]} key={key}>
+          {label}
+        </As>
+      ))}
+    </>
   );
-
-  useEffect(() => {
-    update();
-
-    window.addEventListener('resize', update, { passive: true });
-    return () => window.removeEventListener('resize', update);
-  });
-
-  return { tableRef, targetRef };
 };
 
 export const Table = <T extends { readonly id: string }>({ data, columns }: TableProps<T>) => {
-  const { tableRef, targetRef } = useFloatingTableHeader<HTMLDivElement>();
+  const { tableRef, floatingHeaderRef } = useFloatingTableHeader<HTMLDivElement>();
+
   return (
     <div className={styles.tableWrapper}>
-      <div ref={targetRef} />
+      <div ref={floatingHeaderRef} aria-hidden={true}>
+        <TableHeaderRow columns={columns} as="span" />
+      </div>
       <table ref={tableRef} className={styles.table}>
         <thead>
           <tr>
-            {columns.map(([key, label]) => (
-              <th key={key as string}>{label}</th>
-            ))}
+            <TableHeaderRow columns={columns} as="th" />
           </tr>
         </thead>
         <tbody>
           {data.map((row) => (
             <tr key={row.id}>
               {columns.map(([key]) => (
-                <td key={key as string}>{row[key]}</td>
+                <td key={key}>{row[key]}</td>
               ))}
             </tr>
           ))}
