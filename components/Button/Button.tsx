aa09da1a372444c4732602b0ebaba50646bf3322
Adam Siekierski <a@siekierski.ml>
2021-01-15 17:24:20 +0100

    feat: Usuwanie bloga z poziomu admin panelu (#131)
    * Blog delete UI
    
    * Self-CR
    
    * Button classes order
    
    * Make mutate from useMutation throw errors
    
    * quickfix

--- components/Button/Button.tsx
+++ components/Button/Button.tsx
@@ -15,22 +15,30 @@ type ButtonProps = {
   readonly target?: string;
   readonly rel?: string;
   readonly iconPosition?: 'right' | 'left';
+  readonly buttonStyle?: 'normal' | 'danger';
 };
 
 export const Button = memo<ButtonProps>(
   forwardRef(
     (
-      { as: As = 'button', icon, children, className = '', iconPosition = 'left', ...props },
+      {
+        as: As = 'button',
+        icon,
+        children,
+        className = '',
+        iconPosition = 'left',
+        buttonStyle = 'normal',
+        ...props
+      },
       ref,
     ) => {
       return (
         <As
-          className={clsx(
-            styles.button,
-            className,
-            iconPosition === 'right' && styles.buttonIconRight,
-            props.disabled && styles.buttonDisabled,
-          )}
+          className={clsx(styles.button, className, {
+            [styles.buttonIconRight]: iconPosition === 'right',
+            [styles.buttonDanger]: buttonStyle === 'danger',
+            [styles.buttonDisabled]: props.disabled,
+          })}
           // @ts-ignore
           ref={ref}
           {...props}
--- components/UpdateBlogSection/UpdateBlogSection.tsx
+++ components/UpdateBlogSection/UpdateBlogSection.tsx
@@ -1,5 +1,6 @@
 import { memo } from 'react';
 
+import { DangerZone } from './DangerZone';
 import { UpdateBlogForm } from './UpdateBlogForm';
 import styles from './updateBlogSection.module.css';
 
@@ -10,8 +11,10 @@ type Props = {
 export const UpdateBlogSection = memo<Props>(({ blogId }) => {
   return (
     <section className={styles.section}>
-      <h2 className={styles.heading}>Formularz aktualizacji danych bloga</h2>
+      <h2 className={styles.heading}>Aktualizacja danych</h2>
       <UpdateBlogForm blogId={blogId} />
+      <h2 className={styles.heading}>"Danger zone"</h2>
+      <DangerZone blogId={blogId} />
     </section>
   );
 });
