08e8037a6052a8082ca25305cd2bdba9b18785ac
Bartłomiej Wiśniewski <b.wisniewski128@gmail.com>
2021-01-15 14:52:23 +0100

    Simple version of hits

--- components/AlgoliaSearch/AlgoliaSearch.tsx
+++ components/AlgoliaSearch/AlgoliaSearch.tsx
@@ -1,4 +1,5 @@
 import algoliasearch from 'algoliasearch/lite';
+import { memo } from 'react';
 import { InstantSearch, SearchBox, Hits } from 'react-instantsearch-dom';
 
 const searchClient = algoliasearch(
@@ -6,12 +7,33 @@ const searchClient = algoliasearch(
   process.env.NEXT_PUBLIC_ALGOLIA_API_KEY as string,
 );
 
+type HitProps = {
+  readonly hit: {
+    readonly blogId: string;
+    readonly href: string;
+    readonly publishedAt: string;
+    readonly slug?: string;
+    readonly description?: string;
+    readonly title: string;
+  };
+};
+
+const Hit = memo<HitProps>(({ hit }) => (
+  <>
+    <h3>{hit.title}</h3>
+    <a href={hit.href} target="_blank" rel="noreferrer">
+      Przejdź do artykułu
+    </a>
+  </>
+));
+Hit.displayName = 'AlogliaHit';
+
 export const AlogliaSearch = () => (
   <InstantSearch
     searchClient={searchClient}
     indexName={process.env.NEXT_PUBLIC_ALGOLIA_INDEX_NAME as string}
   >
     <SearchBox />
-    <Hits />
+    <Hits hitComponent={Hit} />
   </InstantSearch>
 );
