c81d58b108cc6915236d1702cbfbca75aa1fbf1b
Adam Siekierski <a@siekierski.ml>
2021-01-15 12:56:23 +0100

    quickfix

--- utils/fetcher.ts
+++ utils/fetcher.ts
@@ -25,7 +25,7 @@ export async function fetcher<S extends AnySchema>(
     });
     if (response.ok) {
       /* eslint-disable @typescript-eslint/no-unsafe-return */
-      return schema.cast(await response.json());
+      return schema.cast(await response.json().catch(() => {}));
     }
     throw new ResponseError(response.statusText, response.status);
   } catch (err) {
