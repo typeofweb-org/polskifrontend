87f37b4ebffe8019a66821459eb96d5e9573d2cb
Bartłomiej Wiśniewski <b.wisniewski128@gmail.com>
2021-01-16 13:35:09 +0100

    Change fading behaviour of hits

--- components/AlgoliaSearch/AlgoliaHit.tsx
+++ components/AlgoliaSearch/AlgoliaHit.tsx
@@ -1,3 +1,5 @@
+import styles from './AlgoliaHit.module.css';
+
 export type Hit = {
   readonly objectID: string;
   readonly href: string;
@@ -7,6 +9,7 @@ export type Hit = {
   readonly title: string;
   readonly blog: {
     readonly name: string;
+    readonly href: string;
     readonly favicon?: string;
   };
 };
@@ -17,7 +20,18 @@ type HitProps = {
 
 export const AlgoliaHit = ({ hit }: HitProps) => (
   <>
-    <h3>{hit.title}</h3>
+    <h3 className={styles.heading}>{hit.title}</h3>
+    <span>
+      <img
+        loading="lazy"
+        src={hit.blog.favicon || undefined}
+        alt=""
+        className={styles.favicon}
+        height={16}
+        width={16}
+      />
+      {hit.blog.name}
+    </span>
     <a href={hit.href} target="_blank" rel="noreferrer noopener">
       Przejdź do artykułu
     </a>
--- components/AlgoliaSearch/AlgoliaSearch.tsx
+++ components/AlgoliaSearch/AlgoliaSearch.tsx
@@ -1,5 +1,6 @@
 import algoliasearch from 'algoliasearch/lite';
-import { useRef, useState } from 'react';
+import type { PointerEventHandler } from 'react';
+import { useCallback, useEffect, useRef, useState } from 'react';
 import { InstantSearch, SearchBox } from 'react-instantsearch-dom';
 
 import { AlgoliaHits } from './AlgoliaHits';
@@ -20,23 +21,41 @@ export const AlogliaSearch = () => {
   const [canShowHits, setCanShowHits] = useState(false);
   const timeoutId = useRef<NodeJS.Timeout | null>(null);
 
-  const handleShowHits = () => {
+  const showHits = useCallback(() => {
     if (timeoutId.current) clearTimeout(timeoutId.current);
     setCanShowHits(true);
-  };
-  const handleHideHits = () => {
+  }, []);
+  const hideHits = useCallback(() => {
     timeoutId.current = setTimeout(() => {
       setCanShowHits(false);
-    }, 1000);
-  };
+    }, 0);
+  }, []);
 
+  useEffect(() => {
+    window.addEventListener('click', hideHits);
+    return () => window.removeEventListener('click', hideHits);
+  }, [hideHits]);
+
+  useEffect(() => {
+    const handleKeyDown = (event: KeyboardEvent) => event.key === 'Escape' && hideHits();
+    window.addEventListener('keydown', handleKeyDown);
+    return () => window.removeEventListener('keydown', handleKeyDown);
+  }, [hideHits]);
+
+  const stopEventPropagation = useCallback<PointerEventHandler<HTMLDivElement>>(
+    (event) => event.stopPropagation(),
+    [],
+  );
   return (
-    <InstantSearch
-      searchClient={searchClient}
-      indexName={process.env.NEXT_PUBLIC_ALGOLIA_INDEX_NAME as string}
-    >
-      <SearchBox onFocus={handleShowHits} onBlur={handleHideHits} />
-      {canShowHits && <AlgoliaHits />}
-    </InstantSearch>
+    // eslint-disable-next-line
+    <div onClick={stopEventPropagation}>
+      <InstantSearch
+        searchClient={searchClient}
+        indexName={process.env.NEXT_PUBLIC_ALGOLIA_INDEX_NAME as string}
+      >
+        <SearchBox onFocus={showHits} onBlur={hideHits} />
+        {canShowHits && <AlgoliaHits />}
+      </InstantSearch>
+    </div>
   );
 };
