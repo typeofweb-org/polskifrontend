820a5f103440e2e77b8e70a80a38a291dbb320ed
Bartłomiej Wiśniewski <b.wisniewski128@gmail.com>
2021-01-17 17:08:09 +0100

    Code review

--- components/AlgoliaSearch/AlgoliaSearch.tsx
+++ components/AlgoliaSearch/AlgoliaSearch.tsx
@@ -1,5 +1,5 @@
 import algoliasearch from 'algoliasearch/lite';
-import { useCallback, useEffect, useRef, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { InstantSearch, SearchBox } from 'react-instantsearch-dom';
 
 import { AlgoliaHits } from './AlgoliaHits';
@@ -9,52 +9,27 @@ const searchClient = algoliasearch(
   process.env.NEXT_PUBLIC_ALGOLIA_API_KEY as string,
 );
 
-declare module 'react-instantsearch-dom' {
-  interface SearchBoxProps {
-    readonly onFocus?: (event: React.SyntheticEvent<HTMLInputElement>) => any;
-    readonly onBlur?: (event: React.SyntheticEvent<HTMLInputElement>) => any;
-  }
-}
-
 export const AlogliaSearch = () => {
-  const [canShowHits, setCanShowHits] = useState(false);
-  const container = useRef<HTMLDivElement | null>(null);
-  const timeoutId = useRef<NodeJS.Timeout | null>(null);
+  const [searchState, setSearchState] = useState<{ readonly query: string }>({ query: '' });
 
-  const showHits = useCallback(() => {
-    if (timeoutId.current) clearTimeout(timeoutId.current);
-    setCanShowHits(true);
-  }, []);
-  const hideHits = useCallback(() => {
-    timeoutId.current = setTimeout(() => {
-      setCanShowHits(false);
-    }, 0);
-  }, []);
+  const showHits = searchState.query !== '';
 
   useEffect(() => {
-    const handleClick = (event: MouseEvent) =>
-      event.target === container.current ||
-      container.current?.contains(event.target as Node) ||
-      hideHits();
-    window.addEventListener('click', handleClick);
-    return () => window.removeEventListener('click', handleClick);
-  }, [hideHits]);
-
-  useEffect(() => {
-    const handleKeyDown = (event: KeyboardEvent) => event.key === 'Escape' && hideHits();
+    const handleKeyDown = (event: KeyboardEvent) =>
+      event.key === 'Escape' && setSearchState({ query: '' });
     window.addEventListener('keydown', handleKeyDown);
     return () => window.removeEventListener('keydown', handleKeyDown);
-  }, [hideHits]);
+  }, [setSearchState]);
 
   return (
-    <div ref={container}>
-      <InstantSearch
-        searchClient={searchClient}
-        indexName={process.env.NEXT_PUBLIC_ALGOLIA_INDEX_NAME as string}
-      >
-        <SearchBox onFocus={showHits} onBlur={hideHits} />
-        {canShowHits && <AlgoliaHits />}
-      </InstantSearch>
-    </div>
+    <InstantSearch
+      searchState={searchState}
+      onSearchStateChange={setSearchState}
+      searchClient={searchClient}
+      indexName={process.env.NEXT_PUBLIC_ALGOLIA_INDEX_NAME as string}
+    >
+      <SearchBox />
+      {showHits && <AlgoliaHits />}
+    </InstantSearch>
   );
 };
