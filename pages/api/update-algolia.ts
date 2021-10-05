d70183f56b309653271dd5cb4e29fdad9c0deebd
Michal Miszczyszyn <michal@mmiszy.pl>
2021-10-05 17:28:14 +0200

    Remove references to secret

--- pages/api/update-algolia.ts
+++ pages/api/update-algolia.ts
@@ -1,109 +1,104 @@
-import Crypto from 'crypto';
+// import Crypto from 'crypto';
 
-import Algoliasearch from 'algoliasearch';
-import * as AllHtmlEntities from 'html-entities';
-import Xss from 'xss';
-import { object, string } from 'yup';
+// import type { Article } from '@prisma/client';
+// import Algoliasearch from 'algoliasearch';
+// import * as AllHtmlEntities from 'html-entities';
+// import Xss from 'xss';
+// import { object, string } from 'yup';
 
-import { withAsync, withDb, withMethods, withValidation } from '../../api-helpers/api-hofs';
-import { getConfig } from '../../api-helpers/config';
+import { withAsync, withDb, withMethods } from '../../api-helpers/api-hofs';
+// import { getConfig } from '../../api-helpers/config';
 
-const client = Algoliasearch(
-  getConfig('NEXT_PUBLIC_ALGOLIA_APP_ID'),
-  getConfig('ALGOLIA_API_SECRET'),
-);
-const algoliaIndex = client.initIndex(getConfig('NEXT_PUBLIC_ALGOLIA_INDEX_NAME'));
+// const client = Algoliasearch(
+//   getConfig('NEXT_PUBLIC_ALGOLIA_APP_ID'),
+//   getConfig('ALGOLIA_API_SECRET'),
+// );
+// const algoliaIndex = client.initIndex(getConfig('NEXT_PUBLIC_ALGOLIA_INDEX_NAME'));
 
-interface Hit {
-  readonly objectID: string;
-  readonly href: string;
-  readonly publishedAt: string;
-  readonly slug?: string | null;
-  readonly description?: string | null;
-  readonly title: string;
-  readonly blog: {
-    readonly name: string;
-    readonly href: string;
-    readonly favicon?: string | null;
-  };
-}
+// interface Hit {
+//   readonly objectID: string;
+//   readonly href: string;
+//   readonly publishedAt: string;
+//   readonly slug?: string | null;
+//   readonly description?: string | null;
+//   readonly title: string;
+//   readonly blog: {
+//     readonly name: string;
+//     readonly href: string;
+//     readonly favicon?: string | null;
+//   };
+// }
 
-function safeCompare(a: string, b: string) {
-  try {
-    return Crypto.timingSafeEqual(Buffer.from(a), Buffer.from(b));
-  } catch {
-    return false;
-  }
-}
+// type InsertPayload = {
+//   readonly type: 'INSERT';
+//   readonly table: string;
+//   readonly schema: string;
+//   readonly record: Article;
+//   readonly old_record: null;
+// };
+
+// type UpdatePayload = {
+//   readonly type: 'UPDATE';
+//   readonly table: string;
+//   readonly schema: string;
+//   readonly record: Article;
+//   readonly old_record: Article;
+// };
+
+// type DeletePayload = {
+//   readonly type: 'DELETE';
+//   readonly table: string;
+//   readonly schema: string;
+//   readonly record: null;
+//   readonly old_record: Article;
+// };
+
+// type Payload = InsertPayload | UpdatePayload | DeletePayload;
 
 export default withAsync(
   withMethods({
-    POST: withValidation({
-      body: object({
-        id: string().required(),
-        event: string().required(),
-      }).required(),
-    })(
-      withDb(async (req) => {
-        if (!req.headers['x-signature'] || typeof req.headers['x-signature'] !== 'string') {
-          return null;
-        }
+    POST: withDb((req) => {
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access -- ok
+      console.log((req as any).body);
 
-        const secret = await req.db.secret.findFirst({
-          where: {
-            name: 'notification_signature',
-          },
-        });
+      // const articleToSubmit = await req.db.article.findFirst({
+      //   where: {
+      //     id: req.body.id,
+      //   },
+      //   select: {
+      //     id: true,
+      //     title: true,
+      //     href: true,
+      //     description: true,
+      //     publishedAt: true,
+      //     slug: true,
+      //     blog: {
+      //       select: {
+      //         name: true,
+      //         href: true,
+      //         favicon: true,
+      //       },
+      //     },
+      //   },
+      // });
 
-        if (!secret) {
-          return null;
-        }
+      // if (!articleToSubmit) {
+      //   return null;
+      // }
 
-        const signature = Crypto.createHmac('sha512', secret.id).update(req._rawBody).digest('hex');
+      // const { id: objectID, description, title, publishedAt, ...rest } = articleToSubmit;
 
-        if (!safeCompare(signature, req.headers['x-signature'])) {
-          return null;
-        }
+      // const hit: Hit = {
+      //   ...rest,
+      //   objectID,
+      //   publishedAt: String(publishedAt),
+      //   description: AllHtmlEntities.decode(
+      //     Xss(description ?? '', { stripIgnoreTag: true, whiteList: {} }),
+      //   ),
+      //   title: AllHtmlEntities.decode(Xss(title, { stripIgnoreTag: true, whiteList: {} })),
+      // };
 
-        const articleToSubmit = await req.db.article.findFirst({
-          where: {
-            id: req.body.id,
-          },
-          select: {
-            id: true,
-            title: true,
-            href: true,
-            description: true,
-            publishedAt: true,
-            slug: true,
-            blog: {
-              select: {
-                name: true,
-                href: true,
-                favicon: true,
-              },
-            },
-          },
-        });
-
-        if (!articleToSubmit) {
-          return null;
-        }
-
-        const { id: objectID, description, title, publishedAt, ...rest } = articleToSubmit;
-
-        const hit: Hit = {
-          ...rest,
-          objectID,
-          publishedAt: String(publishedAt),
-          description: AllHtmlEntities.decode(
-            Xss(description ?? '', { stripIgnoreTag: true, whiteList: {} }),
-          ),
-          title: AllHtmlEntities.decode(Xss(title, { stripIgnoreTag: true, whiteList: {} })),
-        };
-
-        return algoliaIndex.saveObject(hit, { autoGenerateObjectIDIfNotExist: false });
-      }),
-    ),
+      // return algoliaIndex.saveObject(hit, { autoGenerateObjectIDIfNotExist: false });
+    }),
   }),
 );
