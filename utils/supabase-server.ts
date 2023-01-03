8f81fc8820645d596866294ff03d3032a1e9a865
Wojtazzzz <marcin.witas72@gmail.com>
2023-01-03 13:28:22 +0100

    refactor/remove needless function

--- utils/supabase-server.ts
+++
@@ -1,9 +0,0 @@
-import { createServerComponentSupabaseClient } from '@supabase/auth-helpers-nextjs';
-import { headers, cookies } from 'next/headers';
-
-export const createSupabaseServerClient = () => {
-  return createServerComponentSupabaseClient({
-    headers,
-    cookies,
-  });
-};
